f9e4a6ec22f30772278b2a66c1d89c0ee27b88f0
Mako0530 <zhongtiancheng79@gmail.com>
2020-12-24 22:21:53 +0900

    プロフィールページを表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root to: 'articles#index'
 
   resources :articles do
-  resources :comments, only: [:new, :create]
+    resources :comments, only: [:new, :create]
   end
+
+  resource :profile, only: [:show, :edit, :update]
 end
