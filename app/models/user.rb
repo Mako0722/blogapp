ec1935b350c96a90e7831e5e7da4329ee0bc6b04
Mako0530 <zhongtiancheng79@gmail.com>
2021-01-18 21:39:49 +0900

    フォロー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,12 @@ class User < ApplicationRecord
   has_many :favorite_articles, through: :likes, source: :article
   has_one :profile, dependent: :destroy
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
 
   def has_written?(article)
@@ -41,6 +47,22 @@ class User < ApplicationRecord
     profile&.nickname || self.email.split('@').first
   end
 
+  def follow!(user)
+    user_id = get_user_id(user)
+
+    following_relationships.create!(following_id: user_id)
+  end
+
+  def unfollow!(user)
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
+    relation.destroy!
+  end
+
+  def has_followed?(user)
+    following_relationships.exists?(following_id: user.id)
+  end
+
   def prepare_profile
     profile || build_profile
   end
@@ -50,6 +72,15 @@ class User < ApplicationRecord
       profile.avatar
     else
       'default-avatar.png'
+    end
+  end
+
+  private
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
+    else
+      user
     end
   end
 end
